c31ffb0ab030b808feecf31252e06f3f91c9350d
leyealar <leyealar@gmail.com>
2022-11-04 08:43:52 -0500

    Cambios en el hotel dieseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FormareservaComponent } from './formareserva/formareserva.component';
+import { HabitacionesComponent } from './habitaciones/habitaciones.component';
+import { DescripcionComponent } from './descripcion/descripcion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    FormareservaComponent,
+    HabitacionesComponent,
+    DescripcionComponent
   ],
   imports: [
     BrowserModule,
