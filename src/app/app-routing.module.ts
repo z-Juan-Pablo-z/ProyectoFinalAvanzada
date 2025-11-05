a21cb8e0f52fca2c560cf84936342b85000c887e
Juan Florez <juanpabloramirezflorez@gmail.com>
2025-11-05 06:21:06 -0500

    preparacion entrega

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importar tus componentes
+import { HomeComponent } from './home/home.component';
+import { HabitacionListComponent } from './components/habitacion-list/habitacion-list.component';
+import { HabitacionFormComponent } from './components/habitacion-form/habitacion-form.component';
+import { ReservasListComponent } from './components/reserva-list/reserva-list.component';
+//import { FormareservaComponent } from './formareserva/formareserva.component';
+//import { ClienteListComponent } from './components/cliente-list/cliente-list.component';
+//import { ClienteAddComponent } from './components/cliente-add/cliente-add.component';
+//import { ContactoComponent } from './components/contacto/contacto.component';
+
+const routes: Routes = [
+  { path: '', component: HabitacionListComponent }, // Página de inicio
+  { path: 'habitaciones', component: HabitacionListComponent },
+  { path: 'habitacion-add', component: HabitacionFormComponent },
+  { path: 'reservas', component: ReservasListComponent },
+  //{ path: 'reserva-add', component: FormareservaComponent },
+  //{ path: 'clientes', component: ClienteListComponent },
+  //{ path: 'cliente-add', component: ClienteAddComponent },
+  //{ path: 'contacto', component: ContactoComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' } // Redirección a inicio si no encuentra ruta
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes principales
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './home/home.component';
+
+// Módulos funcionales
 import { FormareservaComponent } from './formareserva/formareserva.component';
 import { HabitacionesComponent } from './habitaciones/habitaciones.component';
-import {HttpClientModule} from '@angular/common/http';
-import { HabitacionFormComponent } from './components/habitacion-form/habitacion-form.component'
-import { FormsModule } from '@angular/forms';
+
+// CRUD Habitaciones
+import { HabitacionFormComponent } from './components/habitacion-form/habitacion-form.component';
 import { HabitacionListComponent } from './components/habitacion-list/habitacion-list.component';
-import { ReservasListComponent } from './components/reserva-list/reserva-list.component'; //
+
+// CRUD Reservas
+import { ReservasListComponent } from './components/reserva-list/reserva-list.component';
+
+// 🔹 Aquí puedes añadir Clientes y Contacto cuando los tengas
+// import { ClienteListComponent } from './components/cliente-list/cliente-list.component';
+// import { ClienteAddComponent } from './components/cliente-add/cliente-add.component';
+// import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
@@ -24,13 +38,16 @@ import { ReservasListComponent } from './components/reserva-list/reserva-list.co
     HabitacionesComponent,
     HabitacionFormComponent,
     HabitacionListComponent,
-    ReservasListComponent
+    ReservasListComponent,
+    // ClienteListComponent,
+    // ClienteAddComponent,
+    // ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule,  // 👈 Rutas funcionando
+    FormsModule,       // 👈 Formularios Template Driven
+    HttpClientModule   // 👈 Para consumir tu API
   ],
   providers: [],
   bootstrap: [AppComponent]
